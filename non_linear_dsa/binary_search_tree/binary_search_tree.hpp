4829bb8432a73586ec3570c7c45a5469f20c0658
Reyon Rai <reyonrai50@gmail.com>
2025-10-15 17:40:51 +0545

    Implementing the bst.

--- non_linear_dsa/binary_search_tree/binary_search_tree.hpp
+++ non_linear_dsa/binary_search_tree/binary_search_tree.hpp
@@ -1,15 +1,31 @@
+#pragma once
+#include <stack>
+#include <queue>
+#include <functional>
 
-class bst{
-    private:
-    struct Node{
+class bst
+{
+private:
+    struct Node
+    {
         int data;
-        Node* left;
-        Node* right;
+        Node *left;
+        Node *right;
+        explicit Node(int val) : data(val), left(nullptr), right(nullptr){};
     };
 
-    Node* root;
+    Node *root;
     size_t tree_size;
-    
-    public:
 
-}
+public:
+    bst(): root(nullptr), tree_size(0) {};
+
+    bst( int val ){
+        root = new Node( val );
+        tree_size++;
+    }
+
+    ~bst(){
+        clear( root );
+    }
+};
