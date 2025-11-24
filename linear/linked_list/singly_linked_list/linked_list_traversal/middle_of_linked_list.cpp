5b1eeeb7dcb13a0f9ecf4ba274d63b1012751e32
Reyon Rai <reyonrai50@gmail.com>
2025-11-24 22:21:43 +0545

    Middle of linked list .

---
+++ linear/linked_list/singly_linked_list/linked_list_traversal/middle_of_linked_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+Node* middle_of_linked_list( Node* list ){
+	Node* slow  = list;
+	Node* fast = list;
+
+	while( fast && fast->next ){
+
+	}
+}
