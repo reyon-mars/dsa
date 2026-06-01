e7667439876084df04e3f3459606aaf7373a2b44
Reyon Rai <140488675+reyon-mars@users.noreply.github.com>
2026-06-01 21:39:51 +0545

    Update binary_search_tree.hpp

--- data_structures/non_linear_structures/trees/binary_search_tree/binary_search_tree.hpp
+++ data_structures/non_linear_structures/trees/binary_search_tree/binary_search_tree.hpp
@@ -1,29 +1,46 @@
 #pragma once
-#include <stack>
-#include <queue>
-#include <functional>
+#include <cstddef>
+#include <optional>
+#include <vector>
 
-class bst
-{
+class bst {
+public:
+    bst() = default;
+    ~bst();
+
+    bst(const bst&)            = delete;
+    bst& operator=(const bst&) = delete;
+    bst(bst&&)                 = default;
+    bst& operator=(bst&&)      = default;
+
+    void   insert_node(int val);
+    bool   search(int val)      const;
+    void   delete_node(int val);
+    size_t size()               const;
+    bool   empty()              const;
+
+    std::vector<int> inorder()  const;
+    std::vector<int> preorder() const;
+    std::vector<int> postorder()const;
+
+    std::optional<int> min() const;
+    std::optional<int> max() const;
+
 private:
-    struct Node
-    {
-        int data;
-        Node *left;
-        Node *right;
-        explicit Node(int val) : data(val), left(nullptr), right(nullptr){};
+    struct Node {
+        int   data;
+        Node* left  = nullptr;
+        Node* right = nullptr;
+        explicit Node(int val) : data{val} {}
     };
 
-    Node *root;
-    size_t tree_size;
+    Node*  root      = nullptr;
+    size_t tree_size = 0;
 
-    void insert_node( int val );
-    void clear();
-
-public:
-    
-    bst();
-    bst( int val );
-
-    ~bst();
+    void   destroy(Node* node);
+    Node*  remove(Node* node, int val, bool& removed);
+    Node*  min_node(Node* node) const;
+    void   inorder_walk  (Node* node, std::vector<int>& out) const;
+    void   preorder_walk (Node* node, std::vector<int>& out) const;
+    void   postorder_walk(Node* node, std::vector<int>& out) const;
 };
