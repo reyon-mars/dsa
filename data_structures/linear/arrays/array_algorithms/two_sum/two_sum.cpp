be1db0fecc76d50c04c32d15f680b88a2f3d80fa
Reyon Rai <reyonrai50@gmail.com>
2026-09-12 17:36:40 +0545

    implement solution for the 2 sum problem using unordered_set and only check if the pair exists in a given range.

--- data_structures/linear/arrays/array_algorithms/two_sum/two_sum.cpp
+++ data_structures/linear/arrays/array_algorithms/two_sum/two_sum.cpp
@@ -1,7 +1,9 @@
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
-bool two_sum(std::vector<int>& arr, int target, bool is_sorted = false)
+bool two_sum_2_pointer(std::vector<int>& arr, int target, bool is_sorted = false)
 {
 	if (arr.size() < 2 || arr.empty())
 	{
@@ -27,6 +29,21 @@ bool two_sum(std::vector<int>& arr, int target, bool is_sorted = false)
 		{
 			return true;
 		}
+	}
+	return false;
+}
+
+bool twoSum( std::vector<int>& vec, int target )
+{
+	std::unordered_set<int> seen;
+	seen.reserve( vec.size() );
+	
+	for( const auto num : vec )
+	{
+		if( seen.contains( target - num ) ) 
+			return true;
+
+		seen.insert( num );
 	}
 	return false;
 }
