a7616234ba9f073bad242015e8b4f8889e699ba8
Reyon Rai <reyonrai50@gmail.com>
2026-09-18 17:30:41 +0545

    add linked_list implementation and header file.

---
+++ data_structures/linked_list/singly_linked_list/linked_list.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "node.hpp"
+#include <cstddef>
+
+template < typename T >
+class LinkedList
+{
+    private:
+        int m_count { 0 };
+    
+    public:
+        Node<T>* Head{ nullptr };
+        Node<T>* Tail{ nullptr };
+
+        LinkedList();
+
+        Node<T>* Get( std::size_t index ) const;
+
+        
+        void InsertHead( T val );
+        void InsertTail( T val );
+        void Insert( size_t index, T val );
+
+        int Search( T val ) const;
+        void RemoveHead();
+        void RemoveTail();
+        void RemoveFirst( T val );
+        void Remove( size_t index );
+
+        [[nodiscard]] size_t Count() const;
+        void Print() const;
+};
---
+++ data_structures/linked_list/singly_linked_list/node.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+template < typename T >
+class Node
+{
+    public:
+        T data {};
+        Node<T>* next {};
+
+    Node( T value ) : data( value ), next( nullptr ) {}
+};
